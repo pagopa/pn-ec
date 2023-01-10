18ae861e9ab5fc3901c645bbc81bd6a9fa6201a6
Leonardo Marziali <marzialileonardo862@gmail.com>
2023-01-10 18:18:08 +0100

    Create PresaInCaricoSms microservice

---
+++ microservices/presa-in-carico-sms/src/main/java/it/pagopa/pn/PresaInCaricoSmsApplication.java
@@ -0,0 +1,12 @@
+package it.pagopa.pn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresaInCaricoSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresaInCaricoSmsApplication.class, args);
+    }
+}
