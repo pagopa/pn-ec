e72e9338abecd317a037b10ff73c147a4c231a43
Leonardo Marziali <leonardo.marziali@dgsspa.com>
2023-01-23 12:47:35 +0100

    Add common package - Delete SMS part (il branch è stato staccato da features/invio-sms la parte di SMS verrà mergiata in un'altra pr) - Move spedizionedocumenticartacei package

--- src/main/java/it/pagopa/pn/ec/commons/configuration/AwsConfiguration.java
+++ src/main/java/it/pagopa/pn/ec/commons/configuration/AwsConfiguration.java
@@ -10,8 +10,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
+import software.amazon.awssdk.services.dynamodb.waiters.DynamoDbWaiter;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.SnsClientBuilder;
 
@@ -65,6 +67,16 @@ public class AwsConfiguration {
         }
 
         return dynamoDbClientBuilder.build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient getDynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient).build();
+    }
+
+    @Bean
+    public DynamoDbWaiter getDynamoDbWaiter(DynamoDbClient dynamoDbClient) {
+        return DynamoDbWaiter.builder().client(dynamoDbClient).build();
     }
 
     @Bean
--- src/test/java/it/pagopa/pn/ec/testutils/localstack/LocalStackTestConfig.java
+++ src/test/java/it/pagopa/pn/ec/testutils/localstack/LocalStackTestConfig.java
@@ -1,11 +1,15 @@
 package it.pagopa.pn.ec.testutils.localstack;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.services.dynamodb.waiters.DynamoDbWaiter;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 import static it.pagopa.pn.ec.commons.constant.QueueNameConstant.*;
@@ -14,6 +18,13 @@ import static org.testcontainers.containers.localstack.LocalStackContainer.Servi
 
 @TestConfiguration
 public class LocalStackTestConfig {
+
+//  Oggetti dell'SDK che serviranno per la creazione delle tabelle Dynamo
+    @Autowired
+    private DynamoDbEnhancedClient enhancedClient;
+
+    @Autowired
+    private DynamoDbWaiter dynamoDbWaiter;
 
     static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse(DEFAULT_LOCAL_STACK_TAG)).withServices(
             SQS,
@@ -45,5 +56,28 @@ public class LocalStackTestConfig {
 //      <-- Custom aws services endpoint variables for testing -->
         registry.add("test.aws.dynamodb.endpoint", () -> localStackContainer.getEndpointOverride(DYNAMODB));
         registry.add("test.aws.sns.endpoint", () -> localStackContainer.getEndpointOverride(SNS));
+    }
+
+    @PostConstruct
+    public void createTable() {
+
+        //Esempio di creazione tabelle dynamo all'avvio del container
+
+        /*
+            DynamoDbTable<'Classe che rappresenta lo schema Dynamo'> table = enhancedClient.table('Nome tabella',
+                                                                                               TableSchema.fromBean('Classe che rappresenta lo schema Dynamo'.class));
+
+            table.createTable(builder -> builder.provisionedThroughput(b -> b.readCapacityUnits(5L)
+                                                                                                .writeCapacityUnits(5L)
+                                                                                                .build()));
+
+            // La creazione delle tabelle su Dynamo è asincrona. Bisogna aspettare tramite il DynamoDbWaiter
+
+            ResponseOrException<DescribeTableResponse> response = dynamoDbWaiter.waitUntilTableExists(builder -> builder.tableName(
+                    'Nome tabella').build()).matched();
+            DescribeTableResponse tableDescription = response.response()
+                                                             .orElseThrow(() -> new RuntimeException());
+            // The actual error can be inspected in response.exception()
+         */
     }
 }
