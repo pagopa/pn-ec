0692fa5baa5d3faa8f2716c3c0ad8bf08c722773
Leonardo Marziali <marzialileonardo862@gmail.com>
2023-02-07 11:21:37 +0100

    Add missing files

--- src/main/java/it/pagopa/pn/ec/EcApplication.java
+++ src/main/java/it/pagopa/pn/ec/EcApplication.java
@@ -3,8 +3,21 @@ package it.pagopa.pn.ec;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+
+// <-- COMMONS -->
+// AWS CONFIGURATION
+@PropertySource("classpath:commons/aws-configuration.properties")
+// INTERNAL ENDPOINTS
+@PropertySource("classpath:commons/internal-endpoint.properties")
+
+//  <-- REPOSITORY MANAGER -->
+// DYNAMO TABLES
+@PropertySource("classpath:repositorymanager/repository-manager-dynamo-table.properties")
 @Slf4j
 public class EcApplication {
 
