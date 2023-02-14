74c294752f3f663ab054782bb58a8c4070b6e17e
Leonardo Marziali <marzialileonardo862@gmail.com>
2023-02-14 14:40:53 +0100

    Fix SmsServiceTest

--- src/test/java/it/pagopa/pn/ec/sms/service/SmsServiceTest.java
+++ src/test/java/it/pagopa/pn/ec/sms/service/SmsServiceTest.java
@@ -8,13 +8,10 @@ import it.pagopa.pn.ec.commons.model.dto.NotificationTrackerQueueDto;
 import it.pagopa.pn.ec.commons.rest.call.gestorerepository.GestoreRepositoryCallImpl;
 import it.pagopa.pn.ec.commons.service.SnsService;
 import it.pagopa.pn.ec.commons.service.impl.SqsServiceImpl;
-import it.pagopa.pn.ec.rest.v1.dto.DigitalCourtesySmsRequest;
-import it.pagopa.pn.ec.rest.v1.dto.GeneratedMessageDto;
 import it.pagopa.pn.ec.rest.v1.dto.RequestDto;
 import it.pagopa.pn.ec.sms.configurationproperties.SmsSqsQueueName;
 import it.pagopa.pn.ec.sms.model.pojo.SmsPresaInCaricoInfo;
 import it.pagopa.pn.ec.testutils.annotation.SpringBootTestWebEnv;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,12 +19,9 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.services.sns.model.PublishResponse;
 
-import static it.pagopa.pn.ec.commons.constant.ProcessId.INVIO_SMS;
-import static it.pagopa.pn.ec.rest.v1.dto.DigitalRequestStatus.*;
 import static it.pagopa.pn.ec.sms.testutils.DigitalCourtesySmsRequestFactory.createSmsRequest;
 import static it.pagopa.pn.ec.testutils.constant.EcCommonRestApiConstant.DEFAULT_ID_CLIENT_HEADER_VALUE;
 import static it.pagopa.pn.ec.testutils.constant.EcCommonRestApiConstant.DEFAULT_REQUEST_IDX;
-import static java.time.OffsetDateTime.now;
 import static org.mockito.Mockito.*;
 
 
@@ -55,41 +49,6 @@ class SmsServiceTest {
     private static final SmsPresaInCaricoInfo SMS_PRESA_IN_CARICO_INFO =
             new SmsPresaInCaricoInfo(DEFAULT_REQUEST_IDX, DEFAULT_ID_CLIENT_HEADER_VALUE, createSmsRequest());
 
-    private static final RequestDto REQUEST_IN_BOOKED_STATUS = new RequestDto();
-
-    private static final DigitalCourtesySmsRequest DIGITAL_COURTESY_SMS_REQUEST = SMS_PRESA_IN_CARICO_INFO.getDigitalCourtesySmsRequest();
-
-    private static final NotificationTrackerQueueDto NT_DTO_BOOKED_SENT =
-            new NotificationTrackerQueueDto(SMS_PRESA_IN_CARICO_INFO.getRequestIdx(),
-                                            SMS_PRESA_IN_CARICO_INFO.getXPagopaExtchCxId(),
-                                            now(),
-                                            INVIO_SMS,
-                                            BOOKED.getValue(),
-                                            SENT.getValue(),
-                                            new GeneratedMessageDto());
-
-    private static final NotificationTrackerQueueDto NT_DTO_BOOKED_RETRY =
-            new NotificationTrackerQueueDto(SMS_PRESA_IN_CARICO_INFO.getRequestIdx(),
-                                            SMS_PRESA_IN_CARICO_INFO.getXPagopaExtchCxId(),
-                                            now(),
-                                            INVIO_SMS,
-                                            BOOKED.getValue(),
-                                            RETRY.getValue(),
-                                            new GeneratedMessageDto());
-
-    private static final NotificationTrackerQueueDto NT_DTO_RETRY_SENT =
-            new NotificationTrackerQueueDto(SMS_PRESA_IN_CARICO_INFO.getRequestIdx(),
-                                            SMS_PRESA_IN_CARICO_INFO.getXPagopaExtchCxId(),
-                                            now(),
-                                            INVIO_SMS,
-                                            RETRY.getValue(),
-                                            SENT.getValue(),
-                                            new GeneratedMessageDto());
-
-    @BeforeAll
-    public static void setRequestStatus() {
-        REQUEST_IN_BOOKED_STATUS.setStatusRequest(BOOKED.getValue());
-    }
 
     /**
      * <h3>SMSLR.107.1</h3>
@@ -105,12 +64,12 @@ class SmsServiceTest {
         // TODO: Eliminare il mock una volta sistemato l'ambiente Localstack
         when(snsService.send(anyString(), anyString())).thenReturn(Mono.just(PublishResponse.builder().build()));
 
-        when(gestoreRepositoryCall.getRichiesta(anyString())).thenReturn(Mono.just(REQUEST_IN_BOOKED_STATUS));
+        when(gestoreRepositoryCall.getRichiesta(anyString())).thenReturn(Mono.just( new RequestDto()));
 
         smsService.lavorazioneRichiesta(SMS_PRESA_IN_CARICO_INFO);
 
         verify(gestoreRepositoryCall, times(1)).getRichiesta(SMS_PRESA_IN_CARICO_INFO.getRequestIdx());
-        verify(sqsService, times(1)).send(notificationTrackerSqsName.statoSmsName(), NT_DTO_BOOKED_SENT);
+        verify(sqsService, times(1)).send(eq(notificationTrackerSqsName.statoSmsName()), any(NotificationTrackerQueueDto.class));
     }
 
     /**
@@ -133,15 +92,13 @@ class SmsServiceTest {
         when(snsService.send(anyString(), anyString())).thenReturn(Mono.error(new SnsSendException()))
                                                        .thenReturn(Mono.just(PublishResponse.builder().build()));
 
-        when(gestoreRepositoryCall.getRichiesta(anyString())).thenReturn(Mono.just(REQUEST_IN_BOOKED_STATUS));
+        when(gestoreRepositoryCall.getRichiesta(anyString())).thenReturn(Mono.just( new RequestDto()));
 
         smsService.lavorazioneRichiesta(SMS_PRESA_IN_CARICO_INFO);
 
-        verify(gestoreRepositoryCall, times(1)).getRichiesta(SMS_PRESA_IN_CARICO_INFO.getRequestIdx());
-        verify(snsService, times(2)).send(DIGITAL_COURTESY_SMS_REQUEST.getReceiverDigitalAddress(),
-                                          DIGITAL_COURTESY_SMS_REQUEST.getMessageText());
-        verify(sqsService, times(1)).send(notificationTrackerSqsName.statoSmsName(), NT_DTO_BOOKED_RETRY);
-        verify(sqsService, times(1)).send(notificationTrackerSqsName.statoSmsName(), NT_DTO_RETRY_SENT);
+        verify(gestoreRepositoryCall, times(1)).getRichiesta(anyString());
+        verify(snsService, times(2)).send(anyString(), anyString());
+        verify(sqsService, times(2)).send(eq(notificationTrackerSqsName.statoSmsName()), any(NotificationTrackerQueueDto.class));
     }
 
     /**
@@ -166,14 +123,14 @@ class SmsServiceTest {
         // TODO: Eliminare il mock una volta sistemato l'ambiente Localstack
         when(snsService.send(anyString(), anyString())).thenReturn(Mono.just(PublishResponse.builder().build()));
 
-        when(gestoreRepositoryCall.getRichiesta(anyString())).thenReturn(Mono.just(REQUEST_IN_BOOKED_STATUS));
-        when(sqsService.send(notificationTrackerSqsName.statoSmsName(), NT_DTO_BOOKED_SENT)).thenReturn(Mono.error(new SqsPublishException(
-                notificationTrackerSqsName.statoSmsName())));
+        when(gestoreRepositoryCall.getRichiesta(anyString())).thenReturn(Mono.just( new RequestDto()));
+        when(sqsService.send(eq(notificationTrackerSqsName.statoSmsName()), any(NotificationTrackerQueueDto.class))).thenReturn(Mono.error(
+                new SqsPublishException(notificationTrackerSqsName.statoSmsName())));
 
         smsService.lavorazioneRichiesta(SMS_PRESA_IN_CARICO_INFO);
 
         verify(gestoreRepositoryCall, times(1)).getRichiesta(SMS_PRESA_IN_CARICO_INFO.getRequestIdx());
         verify(sqsService, times(1)).send(eq(notificationTrackerSqsName.statoSmsName()), any(NotificationTrackerQueueDto.class));
-        verify(sqsService, times(1)).send(eq(smsSqsQueueName.errorName()), any(NotificationTrackerQueueDto.class));
+        verify(sqsService, times(1)).send(eq(smsSqsQueueName.errorName()), any(SmsPresaInCaricoInfo.class));
     }
 }
