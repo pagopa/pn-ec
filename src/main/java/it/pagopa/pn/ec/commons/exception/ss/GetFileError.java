880bfdb7a66c38bcd3cfbc12de8833d2b95a0f4f
Leonardo Marziali <marzialileonardo862@gmail.com>
2023-02-18 11:26:43 +0100

    Add metadataOnly query param

--- src/main/java/it/pagopa/pn/ec/commons/exception/ss/GetFileError.java
+++ src/main/java/it/pagopa/pn/ec/commons/exception/ss/GetFileError.java
@@ -3,11 +3,9 @@ package it.pagopa.pn.ec.commons.exception.ss;
 public class GetFileError extends Exception {
 
     public GetFileError(String fileKey, String xPagopaExtchCxId) {
-        super(String.format("""
-                                    Error retrieving attachment '%s' by client '%s'.
-                                    1) Check if the client id on Safe Storage exists
-                                    2) Check that the client id has sufficient read permissions
-                                    3) Check if the attachment exists
-                                    """, fileKey, xPagopaExtchCxId));
+        super(String.format("Error retrieving attachment '%s' by client '%s': check if the client id on Safe Storage exists, check" +
+                            " that the client id has sufficient read permissions, check if the attachment exists",
+                            fileKey,
+                            xPagopaExtchCxId));
     }
 }
--- src/main/java/it/pagopa/pn/ec/commons/rest/call/uribuilder/UriBuilderCallImpl.java
+++ src/main/java/it/pagopa/pn/ec/commons/rest/call/uribuilder/UriBuilderCallImpl.java
@@ -1,31 +1,35 @@
 package it.pagopa.pn.ec.commons.rest.call.uribuilder;
 
-import it.pagopa.pn.ec.commons.exception.ss.GetFileError;
 import it.pagopa.pn.ec.commons.configurationproperties.endpoint.internal.ss.FilesEndpointProperties;
+import it.pagopa.pn.ec.commons.configurationproperties.endpoint.internal.ss.SafeStorageEndpointProperties;
+import it.pagopa.pn.ec.commons.exception.ss.GetFileError;
 import it.pagopa.pn.ec.rest.v1.dto.FileDownloadResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Service
-@Slf4j
 public class UriBuilderCallImpl implements UriBuilderCall {
 
     private final WebClient ssExternalEndpointBasePath;
+    private final SafeStorageEndpointProperties safeStorageEndpointProperties;
     private final FilesEndpointProperties filesEndpointProperties;
 
-    public UriBuilderCallImpl(WebClient ssWebClient, FilesEndpointProperties filesEndpointProperties) {
+    public UriBuilderCallImpl(WebClient ssWebClient, SafeStorageEndpointProperties safeStorageEndpointProperties,
+                              FilesEndpointProperties filesEndpointProperties) {
         this.ssExternalEndpointBasePath = ssWebClient;
+        this.safeStorageEndpointProperties = safeStorageEndpointProperties;
         this.filesEndpointProperties = filesEndpointProperties;
     }
 
     @Override
     public Mono<FileDownloadResponse> getFile(String fileKey, String xPagopaExtchCxId, boolean metadataOnly) {
         return ssExternalEndpointBasePath.get()
-                                         .uri(uriBuilder -> uriBuilder.path(filesEndpointProperties.getFile()).build(fileKey))
-                                         .header("x-pagopa-safestorage-cx-id", xPagopaExtchCxId)
+                                         .uri(uriBuilder -> uriBuilder.path(filesEndpointProperties.getFile())
+                                                                      .queryParam("metadataOnly", metadataOnly)
+                                                                      .build(fileKey))
+                                         .header(safeStorageEndpointProperties.clientHeaderName(), xPagopaExtchCxId)
                                          .retrieve()
                                          .onStatus(HttpStatus::isError,
                                                    clientResponse -> Mono.error(new GetFileError(fileKey, xPagopaExtchCxId)))
