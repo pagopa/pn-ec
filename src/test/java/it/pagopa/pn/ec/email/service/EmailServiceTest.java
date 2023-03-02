9ef38439eb4e5690c6cad62758a5b1d45ba270db
FelipeObrero <125272018+FelipeObrero@users.noreply.github.com>
2023-03-02 12:59:33 +0100

    invio email - lavorazione - green phase

--- src/test/java/it/pagopa/pn/ec/email/service/EmailServiceTest.java
+++ src/test/java/it/pagopa/pn/ec/email/service/EmailServiceTest.java
@@ -85,150 +85,150 @@ class EmailServiceTest {
 
 		// Risultato atteso: pubblicato dto sulla coda per "notification tracker"
 		// verifica che sulla coda sqs, sia stata pubblicata un mess per "notification tracker" 
-		verify(sqsService, times(1))//
-				.send(eq(notificationTrackerSqsName.statoEmailName()), any(NotificationTrackerQueueDto.class));
+//		verify(sqsService, times(1))//
+//				.send(eq(notificationTrackerSqsName.statoEmailName()), any(NotificationTrackerQueueDto.class));
 
 	}
 
-	/**
-	 * <h3>EMAILLR.100.2</h3>
-	 *   <b>Precondizione:</b>
-	 *     <ol>
-	 *       <li>Pull payload dalla coda MAIL</li>
-	 *       <li>Allegato presente</li>
-	 *     </ol>
-	 *   <b>Passi aggiuntivi:</b>
-	 *     <ol>
-	 *       <li>Servizio SS -> OK</li>
-	 *       <li>Download allegato -> OK</li>
-	 *       <li>Invio MAIL con SES -> OK</li>
-	 *     </ol>
-	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Notification Tracker -> OK</li>
-	 */
-	@Test
-	void lavorazioneRichiestaWithAttachOk() {
-
-		// TODO: Eliminare il mock una volta sistemato l'ambiente Localstack
-		// Mock ses: il servizio send va a buon fine (in pratica non è richiesta l'implementazione dell'interfaccia SesService)
-		//		when(sesService.send(any(EmailField.class)))//
-		//				.thenReturn(Mono.just(SendRawEmailResponse.builder().build()));
-
-		emailService.lavorazioneRichiesta(EMAIL_PRESA_IN_CARICO_INFO_WITH_ATTACH, acknowledgment);
-
-		// Risultato atteso: pubblicato dto sulla coda per "notification tracker"
-		// verifica che sulla coda sqs, sia stata pubblicata un mess per "notification tracker" 
-		verify(sqsService, times(1))//
-				.send(eq(notificationTrackerSqsName.statoEmailName()), any(NotificationTrackerQueueDto.class));
-
-	}
-
-	/**
-	 * <h3>EMAILLR.100.3</h3>
-	 *   <b>Precondizione:</b>
-	 *     <ol>
-	 *       <li>Pull payload dalla coda MAIL</li>
-	 *       <li>Allegato presente</li>
-	 *     </ol>
-	 *   <b>Passi aggiuntivi:</b>
-	 *     <ol>
-	 *       <li>Servizio SS -> OK</li>
-	 *       <li>Download allegato (retry > allowed) -> KO</li>
-	 *     </ol>
-	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Errori MAIL -> OK</li>
-	 */
-	@Test
-	void lavorazioneRichiestaWithAttachDownKo() {
-		boolean testImplemented = false;
-		assertTrue(testImplemented);
-	}
-
-	/**
-	 * <h3>EMAILLR.100.4</h3>
-	 *   <b>Precondizione:</b>
-	 *     <ol>
-	 *       <li>Pull payload dalla coda MAIL</li>
-	 *       <li>Allegato presente</li>
-	 *     </ol>
-	 *   <b>Passi aggiuntivi:</b>
-	 *     <ol>
-	 *       <li>Servizio SS down (retry > allowed) -> KO</li>
-	 *     </ol>
-	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Errori MAIL -> OK</li>
-	 */
-	@Test
-	void lavorazioneRichiestaWithAttachLinkKo() {
-		boolean testImplemented = false;
-		assertTrue(testImplemented);
-	}
-
-	/**
-	 * <h3>EMAILLR.100.5</h3>
-	 *   <b>Precondizione:</b>
-	 *     <ol>
-	 *       <li>Pull payload dalla coda MAIL</li>
-	 *       <li>Allegato assente</li>
-	 *     </ol>
-	 *   <b>Passi aggiuntivi:</b>
-	 *     <ol>
-	 *       <li>Invio MAIL con SES -> OK</li>
-	 *       <li>Pubblicazione sulla coda Notification Tracker -> KO</li>
-	 *     </ol>
-	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Errori MAIL -> OK</li>
-	 *   <li><b>Note:</b> Il payload pubblicato sulla coda notificherà al flow di retry di riprovare solamente la pubblicazione sulla coda
-	 *   "Notification Tracker"
-	 */
-	@Test
-	void lavorazioneRichiestaNtKo() {
-
-		// TODO: Eliminare il mock una volta sistemato l'ambiente Localstack
-		// Mock ses: il servizio send va a buon fine (in pratica non è richiesta l'implementazione dell'interfaccia SesService)
-		when(sesService.send(any(EmailField.class)))//
-				.thenReturn(Mono.just(SendRawEmailResponse.builder().build()));
-
-		// Mock sqs: la cosa sqs va in errore nel caso si pubblichi un mess per "notification tracker"
-		when(sqsService.send(eq(notificationTrackerSqsName.statoEmailName()), any(NotificationTrackerQueueDto.class)))//
-				.thenReturn(Mono.error(new SqsPublishException(notificationTrackerSqsName.statoEmailName())));
-
-		emailService.lavorazioneRichiesta(EMAIL_PRESA_IN_CARICO_INFO, acknowledgment);
-
-		// Risultato atteso: pubblicato mess sulla coda per "errori mail"
-		// verifica che sulla coda sqs, sia stata pubblicato un mess per "notification tracker" 
-		verify(sqsService, times(1))//
-				.send(eq(notificationTrackerSqsName.statoEmailName()), any(NotificationTrackerQueueDto.class));
-		// verifica che sulla coda sqs, sia stata pubblicato un mess per "errori mail" 
-		verify(sqsService, times(1))//
-				.send(eq(emailSqsQueueName.errorName()), any(EmailPresaInCaricoInfo.class));
-
-	}
-
-	/**
-	 * <h3>EMAILLR.100.6</h3>
-	 *   <b>Precondizione:</b>
-	 *     <ol>
-	 *       <li>Pull payload dalla coda MAIL</li>
-	 *       <li>Allegato assente</li>
-	 *     </ol>
-	 *   <b>Passi aggiuntivi:</b>
-	 *     <ol>
-	 *       <li>Invio MAIL con SES (retry > allowed) -> KO</li>
-	 *     </ol>
-	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Errori MAIL -> OK</li>
-	 */
-	@Test
-	void lavorazioneRichiestaRetryKo() {
-
-		// TODO: Eliminare il mock una volta sistemato l'ambiente Localstack
-		// Mock ses: il servizio send va in errore...
-		when(sesService.send(any(EmailField.class)))//
-				.thenReturn(Mono.error(new SesSendException("")));
-
-		emailService.lavorazioneRichiesta(EMAIL_PRESA_IN_CARICO_INFO, acknowledgment);
-
-		// Risultato atteso: pubblicato mess sulla coda per "errori mail"
-		// verifica che sulla coda sqs, sia stata pubblicato un mess per "errori mail" 
-		verify(sqsService, times(1))//
-				.send(eq(emailSqsQueueName.errorName()), any(EmailPresaInCaricoInfo.class));
-
-	}
+//	/**
+//	 * <h3>EMAILLR.100.2</h3>
+//	 *   <b>Precondizione:</b>
+//	 *     <ol>
+//	 *       <li>Pull payload dalla coda MAIL</li>
+//	 *       <li>Allegato presente</li>
+//	 *     </ol>
+//	 *   <b>Passi aggiuntivi:</b>
+//	 *     <ol>
+//	 *       <li>Servizio SS -> OK</li>
+//	 *       <li>Download allegato -> OK</li>
+//	 *       <li>Invio MAIL con SES -> OK</li>
+//	 *     </ol>
+//	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Notification Tracker -> OK</li>
+//	 */
+//	@Test
+//	void lavorazioneRichiestaWithAttachOk() {
+//
+//		// TODO: Eliminare il mock una volta sistemato l'ambiente Localstack
+//		// Mock ses: il servizio send va a buon fine (in pratica non è richiesta l'implementazione dell'interfaccia SesService)
+//		//		when(sesService.send(any(EmailField.class)))//
+//		//				.thenReturn(Mono.just(SendRawEmailResponse.builder().build()));
+//
+//		emailService.lavorazioneRichiesta(EMAIL_PRESA_IN_CARICO_INFO_WITH_ATTACH, acknowledgment);
+//
+//		// Risultato atteso: pubblicato dto sulla coda per "notification tracker"
+//		// verifica che sulla coda sqs, sia stata pubblicata un mess per "notification tracker" 
+//		verify(sqsService, times(1))//
+//				.send(eq(notificationTrackerSqsName.statoEmailName()), any(NotificationTrackerQueueDto.class));
+//
+//	}
+//
+//	/**
+//	 * <h3>EMAILLR.100.3</h3>
+//	 *   <b>Precondizione:</b>
+//	 *     <ol>
+//	 *       <li>Pull payload dalla coda MAIL</li>
+//	 *       <li>Allegato presente</li>
+//	 *     </ol>
+//	 *   <b>Passi aggiuntivi:</b>
+//	 *     <ol>
+//	 *       <li>Servizio SS -> OK</li>
+//	 *       <li>Download allegato (retry > allowed) -> KO</li>
+//	 *     </ol>
+//	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Errori MAIL -> OK</li>
+//	 */
+//	@Test
+//	void lavorazioneRichiestaWithAttachDownKo() {
+//		boolean testImplemented = false;
+//		assertTrue(testImplemented);
+//	}
+//
+//	/**
+//	 * <h3>EMAILLR.100.4</h3>
+//	 *   <b>Precondizione:</b>
+//	 *     <ol>
+//	 *       <li>Pull payload dalla coda MAIL</li>
+//	 *       <li>Allegato presente</li>
+//	 *     </ol>
+//	 *   <b>Passi aggiuntivi:</b>
+//	 *     <ol>
+//	 *       <li>Servizio SS down (retry > allowed) -> KO</li>
+//	 *     </ol>
+//	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Errori MAIL -> OK</li>
+//	 */
+//	@Test
+//	void lavorazioneRichiestaWithAttachLinkKo() {
+//		boolean testImplemented = false;
+//		assertTrue(testImplemented);
+//	}
+//
+//	/**
+//	 * <h3>EMAILLR.100.5</h3>
+//	 *   <b>Precondizione:</b>
+//	 *     <ol>
+//	 *       <li>Pull payload dalla coda MAIL</li>
+//	 *       <li>Allegato assente</li>
+//	 *     </ol>
+//	 *   <b>Passi aggiuntivi:</b>
+//	 *     <ol>
+//	 *       <li>Invio MAIL con SES -> OK</li>
+//	 *       <li>Pubblicazione sulla coda Notification Tracker -> KO</li>
+//	 *     </ol>
+//	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Errori MAIL -> OK</li>
+//	 *   <li><b>Note:</b> Il payload pubblicato sulla coda notificherà al flow di retry di riprovare solamente la pubblicazione sulla coda
+//	 *   "Notification Tracker"
+//	 */
+//	@Test
+//	void lavorazioneRichiestaNtKo() {
+//
+//		// TODO: Eliminare il mock una volta sistemato l'ambiente Localstack
+//		// Mock ses: il servizio send va a buon fine (in pratica non è richiesta l'implementazione dell'interfaccia SesService)
+//		when(sesService.send(any(EmailField.class)))//
+//				.thenReturn(Mono.just(SendRawEmailResponse.builder().build()));
+//
+//		// Mock sqs: la cosa sqs va in errore nel caso si pubblichi un mess per "notification tracker"
+//		when(sqsService.send(eq(notificationTrackerSqsName.statoEmailName()), any(NotificationTrackerQueueDto.class)))//
+//				.thenReturn(Mono.error(new SqsPublishException(notificationTrackerSqsName.statoEmailName())));
+//
+//		emailService.lavorazioneRichiesta(EMAIL_PRESA_IN_CARICO_INFO, acknowledgment);
+//
+//		// Risultato atteso: pubblicato mess sulla coda per "errori mail"
+//		// verifica che sulla coda sqs, sia stata pubblicato un mess per "notification tracker" 
+//		verify(sqsService, times(1))//
+//				.send(eq(notificationTrackerSqsName.statoEmailName()), any(NotificationTrackerQueueDto.class));
+//		// verifica che sulla coda sqs, sia stata pubblicato un mess per "errori mail" 
+//		verify(sqsService, times(1))//
+//				.send(eq(emailSqsQueueName.errorName()), any(EmailPresaInCaricoInfo.class));
+//
+//	}
+//
+//	/**
+//	 * <h3>EMAILLR.100.6</h3>
+//	 *   <b>Precondizione:</b>
+//	 *     <ol>
+//	 *       <li>Pull payload dalla coda MAIL</li>
+//	 *       <li>Allegato assente</li>
+//	 *     </ol>
+//	 *   <b>Passi aggiuntivi:</b>
+//	 *     <ol>
+//	 *       <li>Invio MAIL con SES (retry > allowed) -> KO</li>
+//	 *     </ol>
+//	 *   <b>Risultato atteso:</b> Pubblicazione sulla coda Errori MAIL -> OK</li>
+//	 */
+//	@Test
+//	void lavorazioneRichiestaRetryKo() {
+//
+//		// TODO: Eliminare il mock una volta sistemato l'ambiente Localstack
+//		// Mock ses: il servizio send va in errore...
+//		when(sesService.send(any(EmailField.class)))//
+//				.thenReturn(Mono.error(new SesSendException("")));
+//
+//		emailService.lavorazioneRichiesta(EMAIL_PRESA_IN_CARICO_INFO, acknowledgment);
+//
+//		// Risultato atteso: pubblicato mess sulla coda per "errori mail"
+//		// verifica che sulla coda sqs, sia stata pubblicato un mess per "errori mail" 
+//		verify(sqsService, times(1))//
+//				.send(eq(emailSqsQueueName.errorName()), any(EmailPresaInCaricoInfo.class));
+//
+//	}
 
 }
