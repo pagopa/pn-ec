ae58d05b174b1d357f8fbc0614aaea271bf1bb8e
Leonardo Marziali <leonardo.marziali@dgsspa.com>
2023-03-10 12:03:06 +0100

    Implementation of PaperMessageCall interface

--- src/main/java/it/pagopa/pn/ec/commons/rest/call/consolidatore/papermessage/PaperMessageCall.java
+++ src/main/java/it/pagopa/pn/ec/commons/rest/call/consolidatore/papermessage/PaperMessageCall.java
@@ -1,15 +1,16 @@
 package it.pagopa.pn.ec.commons.rest.call.consolidatore.papermessage;
 
+import it.pagopa.pn.ec.commons.rest.call.RestCallException;
 import org.openapitools.client.model.*;
 import reactor.core.publisher.Mono;
 
 public interface PaperMessageCall {
 
 //  <-- ENGAGEMENT -->
-    Mono<OperationResultCodeResponse> putRequest(PaperEngageRequest paperEngageRequest);
-    Mono<OperationResultCodeResponse> putDuplicateRequest(PaperReplicaRequest paperEngageRequest);
+    Mono<OperationResultCodeResponse> putRequest(PaperEngageRequest paperEngageRequest) throws RestCallException.ResourceAlreadyInProgressException;
+    Mono<OperationResultCodeResponse> putDuplicateRequest(PaperReplicaRequest paperReplicaRequest) throws RestCallException.ResourceAlreadyInProgressException;
 
 //  <-- PROGRESS -->
-    Mono<PaperDeliveryProgressesResponse> getProgress(String requestId);
-    Mono<PaperReplicasProgressesResponse> getDuplicateProgress(String requestId);
+    Mono<PaperDeliveryProgressesResponse> getProgress(String requestId) throws RestCallException.ResourceNotFoundException;
+    Mono<PaperReplicasProgressesResponse> getDuplicateProgress(String requestId) throws RestCallException.ResourceNotFoundException;
 }
