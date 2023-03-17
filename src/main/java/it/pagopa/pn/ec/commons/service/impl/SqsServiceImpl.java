4a889779c4cbd6fba2ac17966912df852dea7c09
michelescara <michele.scaramuzzino@gmail.com>
2023-03-17 12:10:42 +0100

    Adaptation of queues to FIFO system

--- src/main/java/it/pagopa/pn/ec/commons/service/impl/SqsServiceImpl.java
+++ src/main/java/it/pagopa/pn/ec/commons/service/impl/SqsServiceImpl.java
@@ -34,38 +34,39 @@ public class SqsServiceImpl implements SqsService {
     @Override
     public <T> Mono<SendMessageResponse> send(final String queueName, final T queuePayload) throws SqsPublishException {
         return Mono.fromCallable(() -> objectMapper.writeValueAsString(queuePayload))
-                   .doOnNext(sendMessageResponse -> log.info("Try to publish on {} with payload {}", queueName, sendMessageResponse))
-                   .zipWith(getQueueUrlFromName(queueName))
-                   .flatMap(objects -> Mono.fromCompletionStage(sqsAsyncClient.sendMessage(builder -> builder.queueUrl(objects.getT2())
-                                                                                                             .messageBody(objects.getT1()))))
-                   .onErrorResume(throwable -> {
-                       log.error(throwable.getMessage(), throwable);
-                       return Mono.error(new SqsPublishException(queueName));
-                   });
+                .doOnNext(sendMessageResponse -> log.info("Try to publish on {} with payload {}", queueName, sendMessageResponse))
+                .zipWith(getQueueUrlFromName(queueName))
+                .flatMap(objects -> Mono.fromCompletionStage(sqsAsyncClient.sendMessage(builder -> builder.queueUrl(objects.getT2())
+                        .messageBody(objects.getT1())
+                        .messageGroupId(QueueUtils.generateMessageGroupId()))))
+                .onErrorResume(throwable -> {
+                    log.error(throwable.getMessage(), throwable);
+                    return Mono.error(new SqsPublishException(queueName));
+                });
     }
 
     @Override
     public <T> Mono<SqsMessageWrapper<T>> getOneMessage(String queueName, Class<T> messageContentClass) {
         return getQueueUrlFromName(queueName).flatMap(queueUrl -> Mono.fromCompletionStage(sqsAsyncClient.receiveMessage(builder -> builder.queueUrl(
-                                                     queueUrl))))
-                                             .flatMap(receiveMessageResponse -> Mono.justOrEmpty(getFirstListElement(receiveMessageResponse.messages())))
-                                             .map(message -> new SqsMessageWrapper<>(message,
-                                                                                     jsonUtils.convertJsonStringToObject(message.body(),
-                                                                                                                         messageContentClass)));
+                        queueUrl))))
+                .flatMap(receiveMessageResponse -> Mono.justOrEmpty(getFirstListElement(receiveMessageResponse.messages())))
+                .map(message -> new SqsMessageWrapper<>(message,
+                        jsonUtils.convertJsonStringToObject(message.body(),
+                                messageContentClass)));
     }
 
     @Override
     public Mono<DeleteMessageResponse> deleteMessageFromQueue(final Message message, final String queueName) {
         return getQueueUrlFromName(queueName).doOnNext(queueUrl -> log.info("Delete message with id {} from {} queue",
-                                                                            message.messageId(),
-                                                                            queueName))
-                                             .flatMap(queueUrl -> Mono.fromCompletionStage(sqsAsyncClient.deleteMessage(builder -> builder.queueUrl(
-                                                     queueUrl).receiptHandle(message.receiptHandle()))));
+                        message.messageId(),
+                        queueName))
+                .flatMap(queueUrl -> Mono.fromCompletionStage(sqsAsyncClient.deleteMessage(builder -> builder.queueUrl(
+                        queueUrl).receiptHandle(message.receiptHandle()))));
     }
 
     @Override
     public Mono<String> getQueueUrlFromName(final String queueName) {
         return Mono.fromCompletionStage(sqsAsyncClient.getQueueUrl(builder -> builder.queueName(queueName)))
-                   .map(GetQueueUrlResponse::queueUrl);
+                .map(GetQueueUrlResponse::queueUrl);
     }
 }
