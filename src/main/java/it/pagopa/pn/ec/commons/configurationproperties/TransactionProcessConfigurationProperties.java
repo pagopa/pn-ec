974c9b4b024323bb7c8638ad2821f307e911ad40
Leonardo Marziali <leonardo.marziali@dgsspa.com>
2023-04-12 16:23:07 +0200

    Fix 404 status validation call

--- src/main/java/it/pagopa/pn/ec/commons/configurationproperties/TransactionProcessConfigurationProperties.java
+++ src/main/java/it/pagopa/pn/ec/commons/configurationproperties/TransactionProcessConfigurationProperties.java
@@ -3,5 +3,6 @@ package it.pagopa.pn.ec.commons.configurationproperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "transaction.process")
-public record TransactionProcessConfigurationProperties(String sms, String smsStartStatus, String email, String emailStartStatus,
-                                                        String pec, String pecStartStatus, String paper, String paperStarterStatus) {}
+public record TransactionProcessConfigurationProperties(String startStatus, String sms, String smsStartStatus, String email,
+                                                        String emailStartStatus, String pec, String pecStartStatus, String paper,
+                                                        String paperStarterStatus) {}
