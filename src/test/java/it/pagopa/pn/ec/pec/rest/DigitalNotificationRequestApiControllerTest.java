f24c63e7f63056c0b399241a904f4928b1cb758c
francesco crisciotti <francesco.crisciotti@dgsspa.com>
2023-04-18 17:43:33 +0200

    fix allineamento main

--- src/test/java/it/pagopa/pn/ec/pec/rest/DigitalNotificationRequestApiControllerTest.java
+++ src/test/java/it/pagopa/pn/ec/pec/rest/DigitalNotificationRequestApiControllerTest.java
@@ -255,5 +255,4 @@ public class DigitalNotificationRequestApiControllerTest {
                                                                                                  .isEqualTo(BAD_REQUEST)
                                                                                                  .expectBody(Problem.class);
     }
-
 }
