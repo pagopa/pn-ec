630c5a1f97e8ef5659c7aa4ca2bdebca64ea4629
damiano.lozzi <damiano.lozzi@dgsspa.com>
2023-10-10 15:27:49 +0200

    created Test case for PN-8187

--- src/test/java/it/pagopa/pn/ec/consolidatore/rest/RicezioneEsitiConsolidatoreControllerTest.java
+++ src/test/java/it/pagopa/pn/ec/consolidatore/rest/RicezioneEsitiConsolidatoreControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 
 import it.pagopa.pn.ec.commons.exception.StatusNotFoundException;
 import it.pagopa.pn.ec.commons.service.AuthService;
@@ -38,7 +39,7 @@ import it.pagopa.pn.ec.commons.service.impl.SqsServiceImpl;
 import it.pagopa.pn.ec.testutils.annotation.SpringBootTestWebEnv;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
-
+import org.hamcrest.Matchers;
 @SpringBootTestWebEnv
 @AutoConfigureWebTestClient
 @Slf4j
@@ -386,6 +387,36 @@ class RicezioneEsitiConsolidatoreControllerTest {
 				.exchange()
 				.expectStatus()
 				.isEqualTo(HttpStatus.BAD_REQUEST);
+
+	}
+
+
+	@Test
+	/** Test PN-8187 */
+	void ricezioneEsitiErroreValidazioneDiscoverAddress() {
+		log.info("RicezioneEsitiConsolidatoreControllerTest.ricezioneEsitiErroreValidazioneStatusCode() : START");
+		when(authService.clientAuth(anyString())).thenReturn(Mono.just(clientConfigurationInternalDto));
+		when(gestoreRepositoryCall.getRichiesta(xPagopaExtchServiceIdHeaderValue, requestId)).thenReturn(Mono.just(getRequestDto()));
+
+		ConsolidatoreIngressPaperProgressStatusEvent progressStatusEvent = getProgressStatusEventWithoutAttachments();
+		progressStatusEvent.setStatusCode(STATUS_CODE_INESISTENTE);
+		progressStatusEvent.setDiscoveredAddress(new ConsolidatoreIngressPaperProgressStatusEventDiscoveredAddress());
+
+		List<ConsolidatoreIngressPaperProgressStatusEvent> events = new ArrayList<>();
+		events.add(progressStatusEvent);
+
+		webClient.put()
+				.uri(RICEZIONE_ESITI_ENDPOINT)
+				.accept(APPLICATION_JSON)
+				.contentType(APPLICATION_JSON)
+				.header(xPagopaExtchServiceIdHeaderName, xPagopaExtchServiceIdHeaderValue)
+				.header(xApiKeyHeaderaName, xApiKeyHeaderValue)
+				.body(BodyInserters.fromValue(events))
+				.exchange()
+				.expectStatus()
+				.isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
+				.expectBody(OperationResultCodeResponse.class)
+				.value(OperationResultCodeResponse::toString, Matchers.containsString("'discoveredAddress.address': rejected value [null]"));
 
 	}
 
