8bc299d62af4cf7b637e92b39dce585ecdce5c9a
michelescara <michelescaramuzzino@gmail.com>
2023-11-22 12:22:16 +0100

    Implemented aruba client proxy.

--- src/main/java/it/pagopa/pn/ec/commons/configuration/http/PecImapBridgeConf.java
+++ src/main/java/it/pagopa/pn/ec/commons/configuration/http/PecImapBridgeConf.java
@@ -2,17 +2,33 @@ package it.pagopa.pn.ec.commons.configuration.http;
 
 import it.pec.bridgews.PecImapBridge;
 import it.pec.bridgews.PecImapBridge_Service;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.frontend.ClientProxyFactoryBean;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.xml.namespace.QName;
 import java.io.IOException;
 
 @Configuration
 public class PecImapBridgeConf {
 
+    @Value("${aruba.server.address}")
+    private String arubaServerAddress;
+
     @Bean
     public PecImapBridge pecImapBridge() throws IOException {
-        return new PecImapBridge_Service(new ClassPathResource("pec/PecImapBridgeBWS1.5.wsdl").getURL()).getPecImapBridgeSOAP();
+        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+        factory.setServiceClass(PecImapBridge.class);
+        factory.setAddress(arubaServerAddress);
+        factory.setWsdlLocation(PecImapBridge_Service.WSDL_LOCATION.getPath());
+        factory.setEndpointName(PecImapBridge_Service.PecImapBridgeSOAP);
+        factory.setServiceName(PecImapBridge_Service.SERVICE);
+        return factory.create(PecImapBridge.class);
     }
 }
