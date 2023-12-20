a0818b8ce7d59a0f3b428edb6af7ef49c36d96c9
michelescara <michelescaramuzzino@gmail.com>
2023-12-20 17:43:18 +0100

    Removed setWsdlLocation from pn-ec and pn-library aruba proxy client initialization.
    Restored aruba.server.address default value.

--- src/main/java/it/pagopa/pn/ec/commons/configuration/http/PecImapBridgeConf.java
+++ src/main/java/it/pagopa/pn/ec/commons/configuration/http/PecImapBridgeConf.java
@@ -3,19 +3,12 @@ package it.pagopa.pn.ec.commons.configuration.http;
 import it.pec.bridgews.PecImapBridge;
 import it.pec.bridgews.PecImapBridge_Service;
 import lombok.CustomLog;
-
-import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.frontend.ClientProxy;
-import org.apache.cxf.frontend.ClientProxyFactoryBean;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.apache.cxf.transport.http.HTTPConduit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import javax.xml.namespace.QName;
-import java.io.IOException;
+import static it.pagopa.pn.ec.commons.utils.LogUtils.INITIALIZING_ARUBA_PROXY_CLIENT;
 
 @Configuration
 @CustomLog
@@ -28,7 +21,7 @@ public class PecImapBridgeConf {
     public PecImapBridge pecImapBridge() {
         var endpointName = PecImapBridge_Service.PecImapBridgeSOAP;
         var serviceName = PecImapBridge_Service.SERVICE;
-        log.debug("ArubaServerAddress : {}, EndpointName : {}, ServiceName : {}", arubaServerAddress, endpointName, serviceName);
+        log.debug(INITIALIZING_ARUBA_PROXY_CLIENT, "pn-ec", arubaServerAddress, endpointName, serviceName);
         JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
         factory.setServiceClass(PecImapBridge.class);
         factory.setAddress(arubaServerAddress);
--- src/main/java/it/pagopa/pn/library/pec/configuration/PecImapBridgeConfiguration.java
+++ src/main/java/it/pagopa/pn/library/pec/configuration/PecImapBridgeConfiguration.java
@@ -3,18 +3,11 @@ package it.pagopa.pn.library.pec.configuration;
 import it.pec.bridgews.PecImapBridge;
 import it.pec.bridgews.PecImapBridge_Service;
 import lombok.CustomLog;
-import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.frontend.ClientProxy;
-import org.apache.cxf.frontend.ClientProxyFactoryBean;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.apache.cxf.transport.http.HTTPConduit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
-import javax.xml.namespace.QName;
-import java.io.IOException;
+import static it.pagopa.pn.ec.commons.utils.LogUtils.INITIALIZING_ARUBA_PROXY_CLIENT;
 
 @Configuration
 @CustomLog
@@ -23,11 +16,13 @@ public class PecImapBridgeConfiguration {
     @Value("${aruba.server.address}")
     private String arubaServerAddress;
     @Bean
-    public PecImapBridge pecImapBridgeClient() throws IOException {
+    public PecImapBridge pecImapBridgeClient() {
+        var endpointName = PecImapBridge_Service.PecImapBridgeSOAP;
+        var serviceName = PecImapBridge_Service.SERVICE;
+        log.debug(INITIALIZING_ARUBA_PROXY_CLIENT, "pn-library.pec", arubaServerAddress, endpointName, serviceName);
         JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
         factory.setServiceClass(PecImapBridge.class);
         factory.setAddress(arubaServerAddress);
-        factory.setWsdlLocation(PecImapBridge_Service.WSDL_LOCATION.getPath());
         factory.setEndpointName(PecImapBridge_Service.PecImapBridgeSOAP);
         factory.setServiceName(PecImapBridge_Service.SERVICE);
         return factory.create(PecImapBridge.class);
