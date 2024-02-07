db8a8715ae1e82821da7fb7f6e4117373f244df9
damiano.lozzi <damiano.lozzi@dgsspa.com>
2024-02-07 16:53:41 +0100

    PN-8907: removed debug logs from ArubaPostacert.getTipo

--- src/main/java/it/pagopa/pn/library/pec/model/pojo/ArubaPostacert.java
+++ src/main/java/it/pagopa/pn/library/pec/model/pojo/ArubaPostacert.java
@@ -11,18 +11,14 @@ import static it.pagopa.pn.ec.scaricamentoesitipec.constant.PostacertTypes.PREAV
 public class ArubaPostacert extends PnPostacert {
     @Override
     public String getTipo() {
-        log.debug("ArubaPostacert.getTipo(): tipo={} , erroreEsteso={}", this.tipo, this.getDati().getErroreEsteso());
         if (tipo.equals(PREAVVISO_ERRORE_CONSEGNA)) {
-            log.debug("ArubaPostacert.getTipo():tipo is PREAVVISO_ERRORE_CONSEGNA, checking erroreEsteso,\n tipo=" + this.tipo);
                 if (this.getDati().getErroreEsteso().startsWith("5.4.1")) {
                 log.debug("ArubaPostacert.getTipo():Errore esteso matches 5.4.1, changing tipo to ERRORE_CONSEGNA, errore esteso:{}", this.getDati().getErroreEsteso());
                 return ERRORE_CONSEGNA;
             } else {
-                log.debug("ArubaPostacert.getTipo():Errore esteso does not match 5.4.1, returning tipo, errore esteso:{}", this.getDati().getErroreEsteso());
                 return this.tipo;
             }
         } else {
-            log.debug("ArubaPostacert.getTipo():tipo is not PREAVVISO_ERRORE_CONSEGNA, returning tipo, tipo=" + this.tipo);
             return this.tipo;
         }
     }
