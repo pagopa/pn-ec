fd5d0663cb19a885bf067105932854d08817fb95
damiano.lozzi <damiano.lozzi@dgsspa.com>
2024-02-07 10:22:22 +0100

    PN-8907: added logging to ArubaPostacert.getTipo()

--- src/main/java/it/pagopa/pn/library/pec/model/pojo/ArubaPostacert.java
+++ src/main/java/it/pagopa/pn/library/pec/model/pojo/ArubaPostacert.java
@@ -2,19 +2,24 @@ package it.pagopa.pn.library.pec.model.pojo;
 
 import it.pagopa.pn.library.pec.pojo.PnPostacert;
 import it.pagopa.pn.library.pec.model.pojo.Postacert;
+import lombok.CustomLog;
 
 import static it.pagopa.pn.ec.scaricamentoesitipec.constant.PostacertTypes.ERRORE_CONSEGNA;
 import static it.pagopa.pn.ec.scaricamentoesitipec.constant.PostacertTypes.PREAVVISO_ERRORE_CONSEGNA;
+@CustomLog
 
 public class ArubaPostacert extends PnPostacert {
-    public ArubaPostacert(Postacert postacert) {
-        super(postacert);
-    }
-
     @Override
     public String getTipo() {
-        if (tipo.equals(PREAVVISO_ERRORE_CONSEGNA) && this.getDati().getErroreEsteso().matches("^5\\.4\\.1.*"))
+        log.debug("ArubaPostacert.getTipo():tipo=" + this.tipo + ",\n erroreEsteso=" + this.getDati().getErroreEsteso());
+        if (tipo.equals(PREAVVISO_ERRORE_CONSEGNA) && this.getDati().getErroreEsteso().matches("^5\\.4\\.1.*")) {
+            log.debug("ArubaPostacert.getTipo():Errore esteso matches 5.4.1, changing tipo to ERRORE_CONSEGNA");
             return ERRORE_CONSEGNA;
-        else return this.tipo;
+        }
+        else {return this.tipo;}
+    }
+
+    public ArubaPostacert(Postacert postacert) {
+        super(postacert);
     }
 }
