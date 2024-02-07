416854a097b9b9890e504017c3346c6c7fbd887e
damiano.lozzi <damiano.lozzi@dgsspa.com>
2024-02-07 16:12:40 +0100

    PN-8907: added logging to ArubaPostacert.getTipo, changed regex condition to startsWith.

--- src/main/java/it/pagopa/pn/library/pec/model/pojo/ArubaPostacert.java
+++ src/main/java/it/pagopa/pn/library/pec/model/pojo/ArubaPostacert.java
@@ -11,13 +11,22 @@ import static it.pagopa.pn.ec.scaricamentoesitipec.constant.PostacertTypes.PREAV
 public class ArubaPostacert extends PnPostacert {
     @Override
     public String getTipo() {
-        log.debug("ArubaPostacert.getTipo():tipo=" + this.tipo + ",\n erroreEsteso=" + this.getDati().getErroreEsteso());
-        if (tipo.equals(PREAVVISO_ERRORE_CONSEGNA) && this.getDati().getErroreEsteso().matches("^5\\.4\\.1.*")) {
-            log.debug("ArubaPostacert.getTipo():Errore esteso matches 5.4.1, changing tipo to ERRORE_CONSEGNA");
-            return ERRORE_CONSEGNA;
+        log.debug("ArubaPostacert.getTipo(): tipo={} , erroreEsteso={}", this.tipo, this.getDati().getErroreEsteso());
+        if (tipo.equals(PREAVVISO_ERRORE_CONSEGNA)) {
+            log.debug("ArubaPostacert.getTipo():tipo is PREAVVISO_ERRORE_CONSEGNA, checking erroreEsteso,\n tipo=" + this.tipo);
+                if (this.getDati().getErroreEsteso().startsWith("5.4.1")) {
+                log.debug("ArubaPostacert.getTipo():Errore esteso matches 5.4.1, changing tipo to ERRORE_CONSEGNA, errore esteso:{}", this.getDati().getErroreEsteso());
+                return ERRORE_CONSEGNA;
+            } else {
+                log.debug("ArubaPostacert.getTipo():Errore esteso does not match 5.4.1, returning tipo, errore esteso:{}", this.getDati().getErroreEsteso());
+                return this.tipo;
+            }
+        } else {
+            log.debug("ArubaPostacert.getTipo():tipo is not PREAVVISO_ERRORE_CONSEGNA, returning tipo, tipo=" + this.tipo);
+            return this.tipo;
         }
-        else {return this.tipo;}
     }
+
 
     public ArubaPostacert(Postacert postacert) {
         super(postacert);
