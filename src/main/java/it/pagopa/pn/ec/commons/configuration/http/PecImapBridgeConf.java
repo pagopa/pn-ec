a7dd5b1b1275a28481a818187cc3894fad50566d
michelescara <michele.scaramuzzino@gmail.com>
2024-02-21 18:26:02 +0100

    PN-9098: Added custom character escape handler to avoid escaping inside CDataTag.

--- src/main/java/it/pagopa/pn/ec/commons/configuration/http/PecImapBridgeConf.java
+++ src/main/java/it/pagopa/pn/ec/commons/configuration/http/PecImapBridgeConf.java
@@ -1,12 +1,17 @@
 package it.pagopa.pn.ec.commons.configuration.http;
 
+import com.sun.xml.bind.marshaller.CharacterEscapeHandler;
 import it.pec.bridgews.PecImapBridge;
 import it.pec.bridgews.PecImapBridge_Service;
 import lombok.CustomLog;
+import org.apache.cxf.jaxb.JAXBDataBinding;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static it.pagopa.pn.ec.commons.utils.LogUtils.INITIALIZING_ARUBA_PROXY_CLIENT;
 
@@ -27,6 +32,15 @@ public class PecImapBridgeConf {
         factory.setAddress(arubaServerAddress);
         factory.setEndpointName(endpointName);
         factory.setServiceName(serviceName);
+
+        //Custom character escape handler to avoid characters escaping inside CDataTag
+        CharacterEscapeHandler characterEscapeHandler = (ch, start, length, isAttVal, out) -> out.write(ch);
+        JAXBDataBinding jaxbDataBinding = new JAXBDataBinding();
+        Map<String, Object> marshallerProperties = new HashMap<>();
+        marshallerProperties.put("com.sun.xml.bind.characterEscapeHandler", characterEscapeHandler);
+        jaxbDataBinding.setMarshallerProperties(marshallerProperties);
+        factory.setDataBinding(jaxbDataBinding);
+
         return factory.create(PecImapBridge.class);
     }
 }
--- src/main/java/it/pagopa/pn/library/pec/configuration/PecImapBridgeConfiguration.java
+++ src/main/java/it/pagopa/pn/library/pec/configuration/PecImapBridgeConfiguration.java
@@ -1,12 +1,17 @@
 package it.pagopa.pn.library.pec.configuration;
 
+import com.sun.xml.bind.marshaller.CharacterEscapeHandler;
 import it.pec.bridgews.PecImapBridge;
 import it.pec.bridgews.PecImapBridge_Service;
 import lombok.CustomLog;
+import org.apache.cxf.jaxb.JAXBDataBinding;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.HashMap;
+import java.util.Map;
+
 import static it.pagopa.pn.ec.commons.utils.LogUtils.INITIALIZING_ARUBA_PROXY_CLIENT;
 
 @Configuration
@@ -25,6 +30,15 @@ public class PecImapBridgeConfiguration {
         factory.setAddress(arubaServerAddress);
         factory.setEndpointName(PecImapBridge_Service.PecImapBridgeSOAP);
         factory.setServiceName(PecImapBridge_Service.SERVICE);
+
+        //Custom character escape handler to avoid characters escaping inside CDataTag
+        CharacterEscapeHandler characterEscapeHandler = (ch, start, length, isAttVal, out) -> out.write(ch);
+        JAXBDataBinding jaxbDataBinding = new JAXBDataBinding();
+        Map<String, Object> marshallerProperties = new HashMap<>();
+        marshallerProperties.put("com.sun.xml.bind.characterEscapeHandler", characterEscapeHandler);
+        jaxbDataBinding.setMarshallerProperties(marshallerProperties);
+        factory.setDataBinding(jaxbDataBinding);
+
         return factory.create(PecImapBridge.class);
     }
 }
