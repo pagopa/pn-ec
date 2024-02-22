49588ea381cf24cc9b4bb09ee10936a199d9d4df
michelescara <michele.scaramuzzino@gmail.com>
2024-02-22 14:53:31 +0100

    PN-9098: Added custom feature to aruba proxy to disable chunked transfer encoding.

--- src/main/java/it/pagopa/pn/ec/commons/configuration/http/PecImapBridgeConf.java
+++ src/main/java/it/pagopa/pn/ec/commons/configuration/http/PecImapBridgeConf.java
@@ -6,14 +6,26 @@ import it.pec.bridgews.PecImapBridge_Service;
 import lombok.CustomLog;
 import org.apache.cxf.jaxb.JAXBDataBinding;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HttpConduitConfig;
+import org.apache.cxf.transport.http.HttpConduitFeature;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
+import org.aspectj.bridge.MessageHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.xml.namespace.QName;
+import javax.xml.ws.handler.Handler;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static it.pagopa.pn.ec.commons.utils.LogUtils.INITIALIZING_ARUBA_PROXY_CLIENT;
+import static org.apache.cxf.helpers.HttpHeaderHelper.CHUNKED;
+import static org.apache.cxf.helpers.HttpHeaderHelper.TRANSFER_ENCODING;
 
 @Configuration
 @CustomLog
@@ -34,12 +46,21 @@ public class PecImapBridgeConf {
         factory.setServiceName(serviceName);
 
         //Custom character escape handler to avoid characters escaping inside CDataTag
-        CharacterEscapeHandler characterEscapeHandler = (ch, start, length, isAttVal, out) -> out.write(ch);
-        JAXBDataBinding jaxbDataBinding = new JAXBDataBinding();
-        Map<String, Object> marshallerProperties = new HashMap<>();
-        marshallerProperties.put("com.sun.xml.bind.characterEscapeHandler", characterEscapeHandler);
-        jaxbDataBinding.setMarshallerProperties(marshallerProperties);
-        factory.setDataBinding(jaxbDataBinding);
+//        CharacterEscapeHandler characterEscapeHandler = (ch, start, length, isAttVal, out) -> out.write(ch);
+//        JAXBDataBinding jaxbDataBinding = new JAXBDataBinding();
+//        Map<String, Object> marshallerProperties = new HashMap<>();
+//        marshallerProperties.put("com.sun.xml.bind.characterEscapeHandler", characterEscapeHandler);
+//        jaxbDataBinding.setMarshallerProperties(marshallerProperties);
+//        factory.setDataBinding(jaxbDataBinding);
+
+        //Custom feature to disable chunked transfer encoding
+        HttpConduitFeature feature = new HttpConduitFeature();
+        HttpConduitConfig httpConduitConfig = new HttpConduitConfig();
+        HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
+        httpClientPolicy.setAllowChunking(false);
+        httpConduitConfig.setClientPolicy(httpClientPolicy);
+        feature.setConduitConfig(httpConduitConfig);
+        factory.setFeatures(List.of(feature));
 
         return factory.create(PecImapBridge.class);
     }
--- src/main/java/it/pagopa/pn/library/pec/configuration/PecImapBridgeConfiguration.java
+++ src/main/java/it/pagopa/pn/library/pec/configuration/PecImapBridgeConfiguration.java
@@ -4,15 +4,37 @@ import com.sun.xml.bind.marshaller.CharacterEscapeHandler;
 import it.pec.bridgews.PecImapBridge;
 import it.pec.bridgews.PecImapBridge_Service;
 import lombok.CustomLog;
+import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.ConduitSelector;
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.feature.Feature;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.interceptor.InterceptorProvider;
 import org.apache.cxf.jaxb.JAXBDataBinding;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.message.Exchange;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.transport.Conduit;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transport.http.HttpConduitConfig;
+import org.apache.cxf.transport.http.HttpConduitFeature;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static it.pagopa.pn.ec.commons.utils.LogUtils.INITIALIZING_ARUBA_PROXY_CLIENT;
+import static org.apache.cxf.helpers.HttpHeaderHelper.CHUNKED;
 
 @Configuration
 @CustomLog
@@ -32,12 +54,21 @@ public class PecImapBridgeConfiguration {
         factory.setServiceName(PecImapBridge_Service.SERVICE);
 
         //Custom character escape handler to avoid characters escaping inside CDataTag
-        CharacterEscapeHandler characterEscapeHandler = (ch, start, length, isAttVal, out) -> out.write(ch);
-        JAXBDataBinding jaxbDataBinding = new JAXBDataBinding();
-        Map<String, Object> marshallerProperties = new HashMap<>();
-        marshallerProperties.put("com.sun.xml.bind.characterEscapeHandler", characterEscapeHandler);
-        jaxbDataBinding.setMarshallerProperties(marshallerProperties);
-        factory.setDataBinding(jaxbDataBinding);
+//        CharacterEscapeHandler characterEscapeHandler = (ch, start, length, isAttVal, out) -> out.write(ch);
+//        JAXBDataBinding jaxbDataBinding = new JAXBDataBinding();
+//        Map<String, Object> marshallerProperties = new HashMap<>();
+//        marshallerProperties.put("com.sun.xml.bind.characterEscapeHandler", characterEscapeHandler);
+//        jaxbDataBinding.setMarshallerProperties(marshallerProperties);
+//        factory.setDataBinding(jaxbDataBinding);
+
+        //Custom feature to disable chunked transfer encoding
+        HttpConduitFeature feature = new HttpConduitFeature();
+        HttpConduitConfig httpConduitConfig = new HttpConduitConfig();
+        HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
+        httpClientPolicy.setAllowChunking(false);
+        httpConduitConfig.setClientPolicy(httpClientPolicy);
+        feature.setConduitConfig(httpConduitConfig);
+        factory.setFeatures(List.of(feature));
 
         return factory.create(PecImapBridge.class);
     }
