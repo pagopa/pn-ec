ce57512d2c4981fb2a555789bba3b9475f41ca65
michelescara <michele.scaramuzzino@gmail.com>
2024-02-27 15:54:15 +0100

    PN-9098: Merged develop into feature/PN-9098

--- src/main/java/it/pagopa/pn/library/pec/exception/aruba/ArubaCallException.java
+++ src/main/java/it/pagopa/pn/library/pec/exception/aruba/ArubaCallException.java
@@ -1,9 +1,20 @@
 package it.pagopa.pn.library.pec.exception.aruba;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ArubaCallException extends RuntimeException {
 
+    private int errorCode;
     public ArubaCallException(String message) {
         super(message);
+    }
+
+    public ArubaCallException(String message, int errorCode) {
+        super(message);
+        this.errorCode = errorCode;
     }
 
 }
--- src/main/java/it/pagopa/pn/library/pec/service/impl/ArubaServiceImpl.java
+++ src/main/java/it/pagopa/pn/library/pec/service/impl/ArubaServiceImpl.java
@@ -1,5 +1,6 @@
 package it.pagopa.pn.library.pec.service.impl;
 
+import it.pagopa.pn.commons.utils.MDCUtils;
 import it.pagopa.pn.library.pec.configurationproperties.ArubaServiceProperties;
 import it.pagopa.pn.library.pec.exception.aruba.ArubaCallException;
 import it.pagopa.pn.library.pec.exception.aruba.ArubaCallMaxRetriesExceededException;
@@ -7,7 +8,6 @@ import it.pagopa.pn.library.pec.model.pojo.ArubaSecretValue;
 import it.pagopa.pn.library.pec.service.ArubaService;
 import it.pec.bridgews.*;
 import lombok.CustomLog;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
@@ -30,6 +30,8 @@ public class ArubaServiceImpl implements ArubaService {
 
     private final ArubaServiceProperties arubaServiceProperties;
 
+    private static final int MESSAGE_NOT_FOUND_ERR_CODE = 99;
+
     @Autowired
     public ArubaServiceImpl(PecImapBridge pecImapBridgeClient, ArubaSecretValue arubaSecretValue, ArubaServiceProperties arubaServiceProperties) {
         this.pecImapBridgeClient = pecImapBridgeClient;
@@ -37,8 +39,13 @@ public class ArubaServiceImpl implements ArubaService {
         this.arubaServiceProperties = arubaServiceProperties;
     }
 
-    private Retry getArubaCallRetryStrategy() {
+    private Retry getArubaCallRetryStrategy(String clientMethodName) {
+        var mdcContextMap = MDCUtils.retrieveMDCContextMap();
         return Retry.backoff(Long.parseLong(arubaServiceProperties.maxAttempts()), Duration.ofSeconds(Long.parseLong(arubaServiceProperties.minBackoff())))
+                .doBeforeRetry(retrySignal -> {
+                    MDCUtils.enrichWithMDC(null, mdcContextMap);
+                    log.debug("Retry number {} for '{}', caused by : {}", retrySignal.totalRetries(), clientMethodName, retrySignal.failure().getMessage(), retrySignal.failure());
+                })
                 .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> {
                     throw new ArubaCallMaxRetriesExceededException();
                 });
@@ -57,7 +64,7 @@ public class ArubaServiceImpl implements ArubaService {
                     } catch (Exception e) {
                         endSoapRequest(sink, e);
                     }
-                })).cast(GetMessageCountResponse.class).retryWhen(getArubaCallRetryStrategy())
+                })).cast(GetMessageCountResponse.class).retryWhen(getArubaCallRetryStrategy(ARUBA_GET_MESSAGE_COUNT))
                 .doOnSuccess(result -> log.info(CLIENT_METHOD_RETURN, ARUBA_GET_MESSAGE_COUNT, result));
     }
 
@@ -65,22 +72,30 @@ public class ArubaServiceImpl implements ArubaService {
     public Mono<DeleteMailResponse> deleteMail(DeleteMail deleteMail) {
         deleteMail.setUser(arubaSecretValue.getPecUsername());
         deleteMail.setPass(arubaSecretValue.getPecPassword());
+        var mdcContextMap = MDCUtils.retrieveMDCContextMap();
         log.debug(CLIENT_METHOD_INVOCATION_WITH_ARGS, ARUBA_DELETE_MAIL, deleteMail);
         return Mono.create(sink -> pecImapBridgeClient.deleteMailAsync(deleteMail, res -> {
                     try {
                         var result = res.get();
                         checkErrors(result.getErrcode(), result.getErrstr());
                         sink.success(result);
+                    } catch (ArubaCallException arubaCallException) {
+                        MDCUtils.enrichWithMDC(null, mdcContextMap);
+                        if (arubaCallException.getErrorCode() == MESSAGE_NOT_FOUND_ERR_CODE) {
+                            log.debug(ARUBA_MESSAGE_MISSING, deleteMail.getMailid());
+                            sink.success();
+                        } else endSoapRequest(sink, arubaCallException);
                     } catch (Exception e) {
+                        MDCUtils.enrichWithMDC(null, mdcContextMap);
                         endSoapRequest(sink, e);
                     }
-                })).cast(DeleteMailResponse.class).retryWhen(getArubaCallRetryStrategy())
+                })).cast(DeleteMailResponse.class).retryWhen(getArubaCallRetryStrategy(ARUBA_DELETE_MAIL))
                 .doOnSuccess(result -> log.info(CLIENT_METHOD_RETURN, ARUBA_DELETE_MAIL, result));
     }
 
     private void checkErrors(Integer errorCode, String errorStr) {
         if (!errorCode.equals(0))
-            throw new ArubaCallException(errorStr);
+            throw new ArubaCallException(errorStr, errorCode);
     }
 
     private void endSoapRequest(MonoSink<Object> sink, Throwable throwable) {
