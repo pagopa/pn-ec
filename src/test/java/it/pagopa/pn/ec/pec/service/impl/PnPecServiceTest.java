3f8bb6367d159ca231907332e4ca31271983b675
damiano.lozzi <damiano.lozzi@dgsspa.com>
2024-03-06 16:54:14 +0100

    PN-8747: fixed test getUnreadMesagesBothEmpty.

--- src/test/java/it/pagopa/pn/ec/pec/service/impl/PnPecServiceTest.java
+++ src/test/java/it/pagopa/pn/ec/pec/service/impl/PnPecServiceTest.java
@@ -248,7 +248,7 @@ class PnPecServiceTest {
 
         StepVerifier.create(combinedMessages)
                 .expectNextMatches(response -> response.getNumOfMessages() == 0
-                        && response.getPnListOfMessages().getMessages().isEmpty())
+                        && response.getPnListOfMessages().getMessages() == null)
                 .expectComplete()
                 .verify();
 
