e7998208890116874d7342cb8b357b12c0b53916
michelescara <michele.scaramuzzino@gmail.com>
2024-03-26 17:02:44 +0100

    PN-10443: added explicit namirial properties set in java System.

--- src/main/java/it/pagopa/pn/library/pec/configuration/PnPecProvidersConf.java
+++ src/main/java/it/pagopa/pn/library/pec/configuration/PnPecProvidersConf.java
@@ -1,14 +1,34 @@
 package it.pagopa.pn.library.pec.configuration;
 
 import com.namirial.pec.library.service.PnPecServiceImpl;
+import lombok.CustomLog;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
 
 @Configuration
+@CustomLog
 public class PnPecProvidersConf {
 
+    @SneakyThrows(IOException.class)
+    private Set<String> getNamirialPropertiesKeySet() {
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("src/main/resources/namirial/namirial.properties"));
+        return prop.stringPropertyNames();
+    }
+
     @Bean
-    public PnPecServiceImpl namirialService() {
+    public PnPecServiceImpl namirialService(@Autowired Environment env) {
+        getNamirialPropertiesKeySet().forEach(key -> {
+            String property = env.getRequiredProperty(key);
+            System.setProperty(key, property);
+        });
         return new PnPecServiceImpl();
     }
 
