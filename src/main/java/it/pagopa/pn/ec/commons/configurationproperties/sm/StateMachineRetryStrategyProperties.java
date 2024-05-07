08a747cfbbfef9546f1a5fa101b262e2c15114ca
michelescara <michele.scaramuzzino@gmail.com>
2024-05-07 16:23:39 +0200

    PN-10717: Fixed statemachine retry strategy property names

--- src/main/java/it/pagopa/pn/ec/commons/configurationproperties/sm/StateMachineRetryStrategyProperties.java
+++ src/main/java/it/pagopa/pn/ec/commons/configurationproperties/sm/StateMachineRetryStrategyProperties.java
@@ -2,7 +2,7 @@ package it.pagopa.pn.ec.commons.configurationproperties.sm;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "pn.ec.sm.retry.strategy")
+@ConfigurationProperties(prefix = "pn.ec.statemachine.retry.strategy")
 public record StateMachineRetryStrategyProperties(Long maxAttempts, Long minBackoff) {
 }
 
