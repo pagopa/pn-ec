7cd8f4d54ad14bb8ca45ebd91cf0819808eb0610
Mario Ottone <mario.ottone@dgsspa.com>
2024-06-28 12:50:16 +0200

    Revert "Revert "Merge branch 'develop' into hotfix/PN-11400""
    This reverts commit 1e0a6312684d28caa8b93cb648a394d56dfa014d.

--- src/test/java/it/pagopa/pn/ec/consolidatore/rest/RicezioneEsitiConsolidatoreControllerTest.java
+++ src/test/java/it/pagopa/pn/ec/consolidatore/rest/RicezioneEsitiConsolidatoreControllerTest.java
@@ -120,6 +120,18 @@ class RicezioneEsitiConsolidatoreControllerTest {
     	return progressStatusEvent;
     }
 
+	private ConsolidatoreIngressPaperProgressStatusEvent getProgressStatusEventWithoutAttachmentsNullIUN() {
+		ConsolidatoreIngressPaperProgressStatusEvent progressStatusEvent = new ConsolidatoreIngressPaperProgressStatusEvent();
+		progressStatusEvent.setRequestId(requestId);
+		progressStatusEvent.setStatusCode(CON010);
+		progressStatusEvent.setStatusDescription(statusCodeDescriptionMap().get(CON010));
+		progressStatusEvent.setStatusDateTime(now);
+		progressStatusEvent.setProductType(PRODUCT_TYPE_AR);
+		progressStatusEvent.setIun(null);
+		progressStatusEvent.setClientRequestTimeStamp(now);
+		return progressStatusEvent;
+	}
+
 	private ConsolidatoreIngressPaperProgressStatusEvent getProgressStatusEventWithCorrectDeliveryFailureCause() {
 		ConsolidatoreIngressPaperProgressStatusEvent progressStatusEvent = new ConsolidatoreIngressPaperProgressStatusEvent();
 		progressStatusEvent.setRequestId(requestId);
@@ -210,7 +222,8 @@ class RicezioneEsitiConsolidatoreControllerTest {
 	}
 
 	private Stream<Arguments> provideArguments() {
-		return Stream.of(Arguments.of(getProgressStatusEventWithAttachments()), Arguments.of(getProgressStatusEventWithoutAttachments()));
+		return Stream.of(Arguments.of(getProgressStatusEventWithAttachments()), Arguments.of(getProgressStatusEventWithoutAttachments()),
+				Arguments.of(getProgressStatusEventWithoutAttachmentsNullIUN()));
 	}
 
 	@ParameterizedTest
