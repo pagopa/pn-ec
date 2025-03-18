5a2a635843b901e8b701df94c549d69f5ff6bdc9
michelescara <michele.scaramuzzino@gmail.com>
2025-03-18 15:41:11 +0100

    PN-14285: Fixed build issues.

--- src/main/java/it/pagopa/pn/ec/configuration/springboot/SpringAnalyzerClientConfig.java
+++ src/main/java/it/pagopa/pn/ec/configuration/springboot/SpringAnalyzerClientConfig.java
@@ -2,9 +2,7 @@ package it.pagopa.pn.ec.configuration.springboot;
 
 import it.pagopa.pn.commons.configs.SpringAnalyzerConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableScheduling
 public class SpringAnalyzerClientConfig extends SpringAnalyzerConfiguration {}
 
