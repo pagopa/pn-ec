6b27b38d52fddb1dfe093b1ac181d0733e8671a5
Fabio Andrea Petrini <51975376+fabioandreapetrini@users.noreply.github.com>
2025-04-01 17:21:53 +0200

    Feature/pn 14432 (#613)
    * PN-14432 Add expiration offset configuration for TTL of the table pn-EcConversioniPDF.
    
    * PN-14432 Add mocking for PdfRasterProperties in tests
    
    ---------
    
    Co-authored-by: Fabio Andrea Petrini <fabioandrea.petrini@noycom.it>

--- src/main/java/it/pagopa/pn/ec/pdfraster/configuration/PdfRasterProperties.java
+++ src/main/java/it/pagopa/pn/ec/pdfraster/configuration/PdfRasterProperties.java
@@ -3,5 +3,5 @@ package it.pagopa.pn.ec.pdfraster.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "pdf-raster")
-public record PdfRasterProperties(Long maxRetryAttempts, Long minRetryBackoff) {
+public record PdfRasterProperties(Long maxRetryAttempts, Long minRetryBackoff, Integer pdfConversionExpirationOffsetInDays) {
 }
