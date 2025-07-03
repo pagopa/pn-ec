98c6639c507c6a0d70c1a8a92b75f37c5e27b6b2
damiano.lozzi <damianolozzi1989@gmail.com>
2025-07-03 11:35:08 +0200

    PN-15215: added toString to AvailabilityManagerDto to correctly log incoming messages

--- src/main/java/it/pagopa/pn/ec/availabilitymanager/model/dto/AvailabilityManagerDto.java
+++ src/main/java/it/pagopa/pn/ec/availabilitymanager/model/dto/AvailabilityManagerDto.java
@@ -3,12 +3,14 @@ package it.pagopa.pn.ec.availabilitymanager.model.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
 @NoArgsConstructor
+@ToString
 public class AvailabilityManagerDto {
     private String version;
     private String id;
