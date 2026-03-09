31b9dc44c8048895cef3d17466bcc6e517d9c6f1
Martina Mattei <martinamattei.mm00@gmail.com>
2026-03-09 11:16:50 +0100

    PN-18355: fix configuration for sns topic and queue

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -25,10 +25,10 @@ SQS_QUEUES=(
   "pn-ec-email-errori-queue.fifo"
   "pn-ec-email-errori-queue-DLQ.fifo"
 
-  "pn-ec-ses-events.fifo"
-  "pn-ec-ses-events-DLQ.fifo"
-  "pn-ec-email-ses-events.fifo"
-  "pn-ec-email-ses-events-DLQ.fifo"
+  "pn-ec-email-ses-events-queue.fifo"
+  "pn-ec-email-ses-events-queue-DLQ.fifo"
+  "pn-ec-email-ses-events-errori-queue-DLQ.fifo"
+  "pn-ec-email-ses-events-errori-queue.fifo"
 
   "pn-ec-tracker-pec-stato-queue.fifo"
   "pn-ec-tracker-pec-errori-queue.fifo"
