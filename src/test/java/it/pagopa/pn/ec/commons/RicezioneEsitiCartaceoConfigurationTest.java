e8c7288ceea9f8d7c8e092ee32f393ee99f85e1f
mdisco-dgs <marco.disco@dgsspa.com>
2026-07-17 11:08:04 +0200

    PN-20639: fix description isDuplicate in yaml and remove unused import

--- src/test/java/it/pagopa/pn/ec/commons/RicezioneEsitiCartaceoConfigurationTest.java
+++ src/test/java/it/pagopa/pn/ec/commons/RicezioneEsitiCartaceoConfigurationTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.Map;
 
 @SpringBootTestWebEnv
