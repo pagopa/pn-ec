91cca398bd9effbb4f8f20cee94e1937051fe98f
michelescara <79908782+michelescara@users.noreply.github.com>
2026-09-22 12:19:27 +0200

    Feature/pn 21395 (#750)
    * PN-21311: propagate printer and du across PaperProgressStatusEvent chain
    
    * Revert "PN-21311: propagate printer and du across PaperProgressStatusEvent chain"
    
    This reverts commit c54aee545f4fc4611411ffdf84d88993e48a3d74.
    
    * PN-21395: add OpenAPI specification for consolidatore proxy and update method names
    
    * PN-21395: fix responses schema for proxy API
    
    * PN-21395: fix proxy API description
    
    * PN-21395: handle consolidatore error responses in paper messages client
    
    * PN-21395: expose consolidatore paper deliveries progresses on external-channel
    
    * PN-21395: add error handling for consolidatore progress requests and configure timeouts
    
    * PN-21395: reduce log redundancy
    
    * PN-21395: resolve Sonar maintainability issues
    
    * PN-21395: handle unsupported media type as non conforming consolidatore response
    
    ---------
    
    Co-authored-by: damiano.lozzi <damianolozzi1989@gmail.com>

--- src/main/java/it/pagopa/pn/ec/commons/exception/cartaceo/ConsolidatoreException.java
+++ src/main/java/it/pagopa/pn/ec/commons/exception/cartaceo/ConsolidatoreException.java
@@ -3,39 +3,113 @@ package it.pagopa.pn.ec.commons.exception.cartaceo;
 import it.pagopa.pn.ec.rest.v1.dto.OperationResultCodeResponse;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.time.Duration;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.TOO_MANY_REQUESTS;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class ConsolidatoreException extends RuntimeException {
     private OperationResultCodeResponse response;
+    private final Integer upstreamStatusCode;
 
-    public ConsolidatoreException(String message, OperationResultCodeResponse response) {
+    public ConsolidatoreException(String message, OperationResultCodeResponse response, Integer upstreamStatusCode) {
         super(message);
         this.response = response;
+        this.upstreamStatusCode = upstreamStatusCode;
+    }
+
+    public ConsolidatoreException(String message, OperationResultCodeResponse response) {
+        this(message, response, null);
     }
 
     public ConsolidatoreException(String message) {
-        super(message);
+        this(message, null, null);
     }
 
     public static class PermanentException extends ConsolidatoreException {
 
+        private static final String PERMANENT_EXCEPTION_MESSAGE = "Permanent exception when calling consolidatore: %s";
+
         public PermanentException(OperationResultCodeResponse operationResultCodeResponse) {
-            super(String.format("Permanent exception when calling consolidatore: %s", operationResultCodeResponse), operationResultCodeResponse);
+            super(String.format(PERMANENT_EXCEPTION_MESSAGE, operationResultCodeResponse), operationResultCodeResponse);
+        }
+
+        public PermanentException(OperationResultCodeResponse operationResultCodeResponse, Integer upstreamStatusCode) {
+            super(String.format(PERMANENT_EXCEPTION_MESSAGE, operationResultCodeResponse),
+                  operationResultCodeResponse,
+                  upstreamStatusCode);
         }
 
         public PermanentException(String message) {
-            super(String.format("Permanent exception when calling consolidatore: %s", message));
+            super(String.format(PERMANENT_EXCEPTION_MESSAGE, message));
+        }
+
+        public PermanentException(String message, Integer upstreamStatusCode) {
+            super(String.format(PERMANENT_EXCEPTION_MESSAGE, message), null, upstreamStatusCode);
         }
     }
 
     public static class TemporaryException extends ConsolidatoreException {
+
+        private static final String TEMPORARY_EXCEPTION_MESSAGE = "Temporary exception when calling consolidatore: %s";
+
         public TemporaryException(OperationResultCodeResponse operationResultCodeResponse) {
-            super(String.format("Temporary exception when calling consolidatore: %s", operationResultCodeResponse), operationResultCodeResponse);
+            super(String.format(TEMPORARY_EXCEPTION_MESSAGE, operationResultCodeResponse), operationResultCodeResponse);
+        }
+
+        public TemporaryException(OperationResultCodeResponse operationResultCodeResponse, Integer upstreamStatusCode) {
+            super(String.format(TEMPORARY_EXCEPTION_MESSAGE, operationResultCodeResponse),
+                  operationResultCodeResponse,
+                  upstreamStatusCode);
         }
 
         public TemporaryException(String message) {
-            super(String.format("Temporary exception when calling consolidatore: %s", message));
+            super(String.format(TEMPORARY_EXCEPTION_MESSAGE, message));
+        }
+
+        public TemporaryException(String message, Integer upstreamStatusCode) {
+            super(String.format(TEMPORARY_EXCEPTION_MESSAGE, message), null, upstreamStatusCode);
+        }
+    }
+
+    public static class RequestIdNotFoundException extends ConsolidatoreException {
+
+        public RequestIdNotFoundException(OperationResultCodeResponse operationResultCodeResponse) {
+            super(String.format("Request id not found on consolidatore: %s", operationResultCodeResponse),
+                  operationResultCodeResponse,
+                  NOT_FOUND.value());
+        }
+    }
+
+    @Getter
+    public static class RateLimitedException extends ConsolidatoreException {
+
+        private final transient Duration retryAfter;
+
+        public RateLimitedException(OperationResultCodeResponse operationResultCodeResponse, Duration retryAfter) {
+            super(String.format("Consolidatore is rate limiting: %s", operationResultCodeResponse),
+                  operationResultCodeResponse,
+                  TOO_MANY_REQUESTS.value());
+            this.retryAfter = retryAfter;
+        }
+
+    }
+
+    public static class CallTimeoutException extends ConsolidatoreException {
+
+        public CallTimeoutException(String message) {
+            super(String.format("Consolidatore did not answer in time: %s", message));
+        }
+    }
+
+    public static class ConnectionFailedException extends ConsolidatoreException {
+
+        public ConnectionFailedException(String message) {
+            super(String.format("Consolidatore is not reachable: %s", message));
         }
     }
 
