18f2d73240a7ff0949b646e6742d822747b55a65
mdisco-dgs <137182394+mdisco-dgs@users.noreply.github.com>
2026-09-22 14:22:01 +0200

    Bug/pn 20799 (#746)
    * PN-20639: end DigitalProgressStatusDto without generatedMessage in pec error and deleted branches
    
    * PN-20881: send DigitalProgressStatusDto without generatedMessage in EMAIL CHANNEL deleted branch
    
    * PN-20882: send DigitalProgressStatusDto without generatedMessage in SMS CHANNEL deleted error branch

--- src/main/java/it/pagopa/pn/ec/sms/service/SmsService.java
+++ src/main/java/it/pagopa/pn/ec/sms/service/SmsService.java
@@ -334,7 +334,7 @@ public class SmsService extends PresaInCaricoService implements QueueOperationsS
             log.debug(MESSAGE_REMOVED_FROM_ERROR_QUEUE, smsSqsQueueName.errorName());
             return sendNotificationOnStatusQueue(smsPresaInCaricoInfo,
                                                  ERROR.getStatusTransactionTableCompliant(),
-                                                 new DigitalProgressStatusDto().generatedMessage(new GeneratedMessageDto())).flatMap(
+                                                 new DigitalProgressStatusDto()).flatMap(
                     sendMessageResponse -> deleteMessageFromErrorQueue(message));
         }
         return Mono.empty();
@@ -403,7 +403,7 @@ public class SmsService extends PresaInCaricoService implements QueueOperationsS
 //                                   Catch errore tirato per lo stato toDelete
 .onErrorResume(StatusToDeleteException.class, exception -> sendNotificationOnStatusQueue(smsPresaInCaricoInfo,
                                      DELETED.getStatusTransactionTableCompliant(),
-                                     new DigitalProgressStatusDto().generatedMessage(new GeneratedMessageDto()))
+                                     new DigitalProgressStatusDto())
         .flatMap(sendMessageResponse -> deleteMessageFromErrorQueue(message))
         .doOnSuccess(result->log.debug(MESSAGE_REMOVED_FROM_ERROR_QUEUE, smsSqsQueueName.errorName())))
 .onErrorResume(internalError -> sendNotificationOnStatusQueue(smsPresaInCaricoInfo,
